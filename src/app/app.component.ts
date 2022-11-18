50ea769ae41538cfa70dca7def5ea025be6c5d76
brunoval <bhsousa@gmail.com>
2022-11-18 18:01:42 -0300

    simple frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private http : HttpClient) {}
   title = 'alticci-angular';
+  number = {value: null};
+  calcutated = 0;
+  isEmpty = true;
+  numberChanged = true;
+
+  calcAlticci() {
+    this.http.get('http://localhost:8080/alticci/' + this.number.value).subscribe((data: any) => {
+      console.log(data)
+      this.calcutated = data;
+      this.numberChanged = false;
+    });
+  }
 }
